359ca4c0f6cbf83565a81dfb00ceaaa65a955efa
SgtStarshine <starshine621@ymail.com>
2018-10-13 21:48:19 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,31 +3,56 @@ var randomWord = ["chocobo", "malboro", "tonberry", "cactuar", "coeurl", "eorzea
 var randomAnswer = randomWord[Math.floor(Math.random() * randomWord.length)];
 
 var str;
+var lives = 10;
 var count = 0;
 var answer = [];
 
 function beginGame() {
-    for (var i = 0; i < randomWord.length; i++) {
-            answer[i] = "_";
+
+    str = document.getElementById("playerAnswer");
+    correct = document.createElement('ul');
+    
+    for (var i = 0; i < randomAnswer.length; i++) {
+        correct.setAttribute('id', 'my-word');
+        guess = document.createElement('li');
+        guess.setAttribute('class', 'guess');
+
+        guess.innerHTML = "_";
+
+        answer.push(guess);
+        str.appendChild(correct);
+        correct.appendChild(guess);
     }
-
-    str = answer.join(" ");
-    document.getElementById("playerAnswer").innerHTML = str;
-
-
 }
 
-function guessAnswer() {
-    var letter = document.getElementById("letter").value;
+document.onkeyup = function(event) {
+    var choice = event.key;
 
-    if (letter.length > 0) {
+    if (choice.length > 0) {
         for (var i = 0; i < randomAnswer.length; i++) {
-            if (randomAnswer[i] === letter) {
-                answer[i] = letter;
+            if (randomAnswer[i] === choice) {
+                answer[i] = choice;
+                count++;
             }
         }
-        count++;
-        document.getElementById("counter").innerHTML = "No. of guesses: " + count;
+
+        var j = (randomAnswer.indexOf(choice));
+        
+        if (j === -1) {
+            lives -= 1;
+        }
+        
         document.getElementById("playerAnswer").innerHTML = answer.join(" ");
+        document.getElementById("counter").innerHTML = "No. of correct guesses: " + count;
+        document.getElementById("myLives").innerHTML = "You have " + lives + " lives";
+        
+        if (lives < 1) {
+            document.getElementById("myLives").innerHTML = "Game Over";
+        }
+        for (var i = 0; i < answer.length; i++) {
+            if (count === answer.length) {
+                document.getElementById("myLives").innerHTML = "You Win!";
+            }
+        }
     }
 }
